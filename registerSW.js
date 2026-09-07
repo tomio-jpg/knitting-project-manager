87aade78bb7adb35e67bedd5ba87adab0e095517
tomio-jpg <tomio-jpg@users.noreply.github.com>
2026-09-07 23:50:29 +0900

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/knitting-project-manager/sw.js', { scope: '/knitting-project-manager/' })})}
